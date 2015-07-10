e05d5da79310f0b4e7bda6c2ca7d65e9f2903098
marcoviappiani <marco.viappiani@gmail.com>
2015-07-10 15:52:08 -0700

    Make enemies appear on canvas

--- client/watchout.js
+++ client/watchout.js
@@ -11,38 +11,87 @@
 var gameOptions = {height: 450,
                    width: 700,
                    numOfEnemies: 30,
-                   enemyRadiusSize: 10}
+                   enemyRadiusSize: 10};
 
 var enemies = [];
 
 var populateEnemies = function(){
   for (var i = 0; i < gameOptions.numOfEnemies; i++){
-    enemies.push(makeRandomEnemy());
+    enemies.push(makeRandomEnemy(i));
+
   }
 }
 
-var makeRandomEnemy = function(){
+var makeRandomEnemy = function(key){
   var xPos = getRandomInt(50, gameOptions.width - 50);
   var yPos = getRandomInt(50, gameOptions.height - 50);
   var radius = gameOptions.enemyRadiusSize;
   return {
     xPos: xPos,
     yPos: yPos,
-    radius: radius
+    radius: radius,
+    key: key
   }
-}
+};
 
 var getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var gameArea = d3.select(".gameArea")
+var gameArea = d3.select(".container")
     .append('svg:svg')
     .attr('width', gameOptions.width)
-    .attr('height', gameOptions.height);
+    .attr('height', gameOptions.height)
+    .attr('class', 'gameArea');
+
+
+var update = function(data) {
+
+  // DATA JOIN
+  // Join new data with old elements, if any.
+  var currentEnemies = gameArea.selectAll('circle')
+      .data(enemies, function(d){return d.key;});
+
+  // UPDATE
+  // Update old elements as needed.
+  currentEnemies
+      .attr("cx", function(d){return d.xPos;})
+      .attr("cy", function(d){return d.yPos;});
+
+  // ENTER
+  // Create new elements as needed.
+  currentEnemies.enter().append("circle")
+      .attr("class", "enemy")
+      .attr("fill", "red")
+      .attr("cx", function(d){return d.xPos;})
+      .attr("cy", function(d){return d.yPos;})
+      .attr("r", function(d){return d.radius;});
+
+// function(d) { return d; } Access elements like this...
+
+  // ENTER + UPDATE
+  // Appending to the enter selection expands the update selection to include
+  // entering elements; so, operations on the update selection after appending to
+  // the enter selection will apply to both entering and updating nodes.
+  //currentEnemies.attr("x", function(d, i) { return i * 32; })
+
+  // EXIT
+  // Remove old elements as needed.
+  // currentEnemies.exit().remove();
+}
+
+var updatePositions = function(){
+  for (var i = 0; i < enemies.length; i++){
+    // enemies[i].yPos = 
+    // enemies[i].xPos = 
+  }
+  update(enemies);
+}
 
 
 populateEnemies();
-console.log(enemies);
+update(enemies);
 
-
+setInterval(function(){
+  updatePositions();
+}, 5000);
